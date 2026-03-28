70866a4f6d0da3b0c292d4f9b989eaf450d0105a
ArnobAlam <arnob.alam10@gmail.com>
2026-03-28 21:59:40 +0600

    Update Supabase credentials to new project

--- client/src/components/CelebrityReviews.jsx
+++ client/src/components/CelebrityReviews.jsx
@@ -5,8 +5,8 @@ import { createClient } from '@supabase/supabase-js';
 
 // Uses the same Supabase project as Admin
 const supabase = createClient(
-  'https://efijepzbnhnslljvpakc.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmaWplcHpibmhuc2xsanZwYWtjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE1NzY4OTEsImV4cCI6MjA4NzE1Mjg5MX0.c6YEmjWZgPp2xifpAFYibtyaMkXHHYfULCsAvS5DpAA'
+  'https://vbcvbruvqvbvkouiutdz.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiY3ZicnV2cXZidmtvdWl1dGR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3MDc0MjAsImV4cCI6MjA5MDI4MzQyMH0.CB8aj7-K7br2CN3MJgF7dbIUZfT4CMnLYjFVM4aZ5KQ'
 );
 
 // Shown while Supabase loads / if the table is empty
